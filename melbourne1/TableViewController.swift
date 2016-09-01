591d148b887b61029781f0d68f89e3d89cef96df
JeremyWangAU <zihaowang@dyn-118-139-24-138.its.monash.edu.au>
2016-09-01 10:50:08 +1000

    new

--- melbourne1/TableViewController.swift
+++ melbourne1/TableViewController.swift
@@ -11,14 +11,59 @@ import Firebase
 import CoreLocation
 
 class TableViewController: UITableViewController, CLLocationManagerDelegate {
+    var flag : Bool?
+    var index = 1;
+    let searchController = UISearchController(searchResultsController:nil)
+    @IBOutlet weak var allItemT: UIBarButtonItem!
+    
+    @IBAction func allItem(sender: AnyObject) {
+        if self.index%2 != 0{
+            self.flag = true
+            artworks.removeAll()
+            self.tableView.reloadData()
+            fetchArtworks()
+            self.allItemT.title="Nearby"
+            self.navigationItem.title = "ALL"
+            self.index = index + 1
+        }
+        else
+        {
+            self.flag = false
+            artworks.removeAll()
+            self.tableView.reloadData()
+            fetchArtworks()
+            self.allItemT.title="All"
+            self.navigationItem.title = "Nearby"
+            self.index = index + 1
+        }
 
+    }
+    
+    
+    
     var artworks = [Artworks]()
+    var filteredArtwork = [Artworks]()
+    func filterContentForSearchText(searchText:String,scope:String = "All"){
+        filteredArtwork = artworks.filter{
+            artwork in
+            return artwork.Name!.lowercaseString.containsString(searchText.lowercaseString) || artwork.Date!.containsString(searchText) ||
+                artwork.Artist!.lowercaseString.containsString(searchText.lowercaseString)
+        }
+        tableView.reloadData()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-
         
+        
+        
+        self.flag = false
         fetchArtworks()
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
         
     }
     func fetchArtworks(){
@@ -28,11 +73,6 @@ class TableViewController: UITableViewController, CLLocationManagerDelegate {
         locationManager1.desiredAccuracy = kCLLocationAccuracyBest
         locationManager1.requestWhenInUseAuthorization()
         locationManager1.startUpdatingLocation()
-
-        
-    
-        
-        
         
         let locValue:CLLocationCoordinate2D = locationManager1.location!.coordinate
         
@@ -41,12 +81,10 @@ class TableViewController: UITableViewController, CLLocationManagerDelegate {
         //let initialLocation = CLLocation(latitude: -37.8885677, longitude: 145.045028)
         let currentlocation = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
         //let i111 = self.locationManager1.location
-       
-
+        
         
         let ref = Firebase(url: "https://melbourne-footprint.firebaseio.com/")
-      
-
+        
         ref.childByAppendingPath("Heritage").observeEventType(.ChildAdded, withBlock: { (snapshot) in
             if let dictionary = snapshot.value as? [String: AnyObject]
             {
@@ -55,11 +93,11 @@ class TableViewController: UITableViewController, CLLocationManagerDelegate {
                 
                 let fullNameArr = artwork.Coordinates!.componentsSeparatedByString(",")
                 
-                var firstName: String = fullNameArr[0]
-                var lastName: String = fullNameArr[1]
+                let firstName: String = fullNameArr[0]
+                let lastName: String = fullNameArr[1]
                 
-                var latitude1 = String(firstName.characters.dropFirst())
-                var longtitude1 = String(lastName.characters.dropLast())
+                let latitude1 = String(firstName.characters.dropFirst())
+                let longtitude1 = String(lastName.characters.dropLast())
                 
                 
                 let latitude2 = (latitude1  as NSString).doubleValue
@@ -68,89 +106,94 @@ class TableViewController: UITableViewController, CLLocationManagerDelegate {
                 let initialLocation = CLLocation(latitude: latitude2, longitude: longitude2)
                 
                 let distance = currentlocation.distanceFromLocation(initialLocation)
-              
-                
-                if distance < 1500{
-                self.artworks.append(artwork)
-                
+                if self.flag == true{
+                    self.artworks.append(artwork)
                 }
-                
-                for var i = 0; i < self.artworks.count; i++ {
-                    for var j = 1; j < self.artworks.count-i; j++ {
+                else if self.flag == false{
+                    if distance < 1500{
+                        self.artworks.append(artwork)
                         
-                        
-                        
-                        
-                        let full = self.artworks[j-1].Coordinates!.componentsSeparatedByString(",")
-                        
-                        var first: String = full[0]
-                        var last: String = full[1]
-                        
-                        var lat1 = String(first.characters.dropFirst())
-                        var long1 = String(last.characters.dropLast())
-                        
-                        
-                        let lat2 = (lat1 as NSString).doubleValue
-                        let long2 = (long1 as NSString).doubleValue
-                        
-                        let LocationJx = CLLocation(latitude: lat2, longitude: long2)
-                        
-                        let distanceJx = currentlocation.distanceFromLocation(LocationJx) as Double
-                        
-                        let fullj = self.artworks[j].Coordinates!.componentsSeparatedByString(",")
-                        
-                        var firstj: String = fullj[0]
-                        var lastj: String = fullj[1]
-                        
-                        var lat1j = String(firstj.characters.dropFirst())
-                        var long1j = String(lastj.characters.dropLast())
-                        
-                        
-                        let lat2j = (lat1j  as NSString).doubleValue
-                        let long2j = (long1j as NSString).doubleValue
-                        
-                        let LocationJ = CLLocation(latitude: lat2j, longitude: long2j)
-                        
-                        let distanceJ = currentlocation.distanceFromLocation(LocationJ) as Double
-                        
-                        
-                        if distanceJ < distanceJx {
-                            let swap = self.artworks[j-1]
-                            self.artworks[j-1] = self.artworks[j]
-                            self.artworks[j] = swap
+                        for i in 0 ..< self.artworks.count {
+                            for j in 1 ..< self.artworks.count-i {
+                                
+                                
+                                
+                                
+                                let full = self.artworks[j-1].Coordinates!.componentsSeparatedByString(",")
+                                
+                                let first: String = full[0]
+                                let last: String = full[1]
+                                
+                                let lat1 = String(first.characters.dropFirst())
+                                let long1 = String(last.characters.dropLast())
+                                
+                                
+                                let lat2 = (lat1 as NSString).doubleValue
+                                let long2 = (long1 as NSString).doubleValue
+                                
+                                let LocationJx = CLLocation(latitude: lat2, longitude: long2)
+                                
+                                let distanceJx = currentlocation.distanceFromLocation(LocationJx) as Double
+                                
+                                let fullj = self.artworks[j].Coordinates!.componentsSeparatedByString(",")
+                                
+                                let firstj: String = fullj[0]
+                                let lastj: String = fullj[1]
+                                
+                                let lat1j = String(firstj.characters.dropFirst())
+                                let long1j = String(lastj.characters.dropLast())
+                                
+                                
+                                let lat2j = (lat1j  as NSString).doubleValue
+                                let long2j = (long1j as NSString).doubleValue
+                                
+                                let LocationJ = CLLocation(latitude: lat2j, longitude: long2j)
+                                
+                                let distanceJ = currentlocation.distanceFromLocation(LocationJ) as Double
+                                
+                                
+                                if distanceJ < distanceJx {
+                                    let swap = self.artworks[j-1]
+                                    self.artworks[j-1] = self.artworks[j]
+                                    self.artworks[j] = swap
+                                }
+                            }
                         }
                     }
                 }
                 
-                
                 //self.artworks.removeAll()
                 dispatch_async(dispatch_get_main_queue(),{self.tableView.reloadData() } )
-        }
-        }, withCancelBlock: nil)
-    
+            }
+            }, withCancelBlock: nil)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        if searchController.active && searchController.searchBar.text != "" {
+            return filteredArtwork.count
+        }
+        
         return artworks.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath) as! TableViewCell
-
+        
         
         let locationManager1 = CLLocationManager()
         locationManager1.delegate = self
@@ -158,7 +201,7 @@ class TableViewController: UITableViewController, CLLocationManagerDelegate {
         locationManager1.requestWhenInUseAuthorization()
         locationManager1.startUpdatingLocation()
         let locValue:CLLocationCoordinate2D = locationManager1.location!.coordinate
-      
+        
         let currentlocation = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
         
         
@@ -170,8 +213,15 @@ class TableViewController: UITableViewController, CLLocationManagerDelegate {
         cell.addressL.numberOfLines = 0;
         cell.nameL.lineBreakMode = NSLineBreakMode.ByWordWrapping
         cell.nameL.numberOfLines = 0;
-
-        let artwork = artworks[indexPath.row]
+        
+        
+        let artwork : Artworks
+        if searchController.active && searchController.searchBar.text != "" {
+            artwork = filteredArtwork[indexPath.row]
+        }else {
+            artwork = artworks[indexPath.row]
+        }
+        
         cell.nameL.text = artwork.Name
         
         let fullNameArr = artwork.Coordinates!.componentsSeparatedByString(",")
@@ -187,7 +237,7 @@ class TableViewController: UITableViewController, CLLocationManagerDelegate {
         let longitude2 = (longtitude1 as NSString).doubleValue
         
         let initialLocation = CLLocation(latitude: latitude2, longitude: longitude2)
-
+        
         let distance = currentlocation.distanceFromLocation(initialLocation)
         let doubleDis : Double = distance
         let intDis : Int = Int(doubleDis)
@@ -195,64 +245,80 @@ class TableViewController: UITableViewController, CLLocationManagerDelegate {
         cell.addressL.text = "\(intDis)m"
         if let photo = artwork.Photo{
             cell.tableImageView.loadImageUsingCacheWithUrlString(photo)
-
+            print(photo)
+            
         }
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-   
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "viewArtwork"
         {
-        
+            
             let indexPath = self.tableView.indexPathForSelectedRow!
             let controller: ViewDetailsController = segue.destinationViewController
-            as! ViewDetailsController
-            controller.currentArtwork = self.artworks[indexPath.row]
-            //self.tabBarController?.tabBar.hidden = true
+                as! ViewDetailsController
+            let artworkDetail: Artworks
+            if searchController.active && searchController.searchBar.text != "" {
+                artworkDetail = filteredArtwork[indexPath.row]
+            }else {
+                artworkDetail = artworks[indexPath.row]
+            }
+            
+            
+            controller.currentArtwork = artworkDetail            //self.tabBarController?.tabBar.hidden = true
             controller.hidesBottomBarWhenPushed = true
         }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
+    
+}
 
+extension TableViewController : UISearchResultsUpdating {
+    
+    func updateSearchResultsForSearchController(searchController:UISearchController)
+    {
+        filterContentForSearchText(searchController.searchBar.text!)
+}
 }
